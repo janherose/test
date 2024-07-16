521773741035094d114b2dcf46eaab4e21e4dc98
janherose <janhero1dok@gmail.com>
2024-07-17 02:21:46 +0700

    Create test.sol

---
+++ test.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier:
+// test
+pragma solidity >=0.7.0 <0.9.0;
+
+library Math {
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b);
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a);
+
+        return c;
+    }
+
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+}
